27d2750fc7c4e40efd306dcb8c6e2013379b5c42
Basith Kunimal <basith374@gmail.com>
2021-08-09 07:33:06 +0530

    eslint

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,26 +5,26 @@ import Biddings from "../pages/Biddings";
 import CreateBidding from "../pages/CreateBidding";
 
 export default function Dashboard() {
-  return <div className="flex flex-wrap bg-gray-100 w-full h-screen">
-    <Sidebar />
+  return (
+    <div className="flex flex-wrap bg-gray-100 w-full h-screen">
+      <Sidebar />
 
-    <div className="w-9/12">
-      <div className="p-4 text-gray-500">
-        <Switch>
-          <Route path="/biddings/new">
-            <CreateBidding />
-          </Route>
-          <Route path="/biddings">
-            <Biddings />
-          </Route>
-          <Route path="/users">
-            users
-          </Route>
-          <Route path="/" exact>
-            <Table />
-          </Route>
-        </Switch>
+      <div className="w-9/12">
+        <div className="p-4 text-gray-500">
+          <Switch>
+            <Route path="/biddings/new">
+              <CreateBidding />
+            </Route>
+            <Route path="/biddings">
+              <Biddings />
+            </Route>
+            <Route path="/users">users</Route>
+            <Route path="/" exact>
+              <Table />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }
