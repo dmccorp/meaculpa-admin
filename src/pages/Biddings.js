dc82c3c7b2f7b44b6915d15d82676cff0f3c59ba
Basith Kunimal <basith374@gmail.com>
2021-08-17 07:57:56 +0530

    fix table

--- src/pages/Biddings.js
+++ src/pages/Biddings.js
@@ -17,8 +17,8 @@ function Table() {
 
   return (
     <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div className="">
+        <div className="">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
@@ -88,7 +88,7 @@ function Table() {
                         </div>
                       </div>
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
+                    <td className="px-6 py-4">
                       <div className="text-sm text-gray-900">
                         {item.productname}
                       </div>
