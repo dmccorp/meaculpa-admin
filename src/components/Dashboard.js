2360bbda577334eadaf1b605132671a356c990f7
Arakkan <jarakkan@paypal.com>
2021-08-17 10:57:51 +0530

    Added edit functionality for product

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Biddings from "../pages/Biddings";
 import CreateBidding from "../pages/CreateBidding";
 import Products from "../pages/Products";
+import EditProduct from "../pages/EditProduct";
 import CreateProduct from "../pages/CreateProducts";
 
 export default function Dashboard() {
@@ -14,6 +15,9 @@ export default function Dashboard() {
       <div className="w-9/12">
         <div className="p-4 text-gray-500">
           <Switch>
+            <Route path="/product/edit/:id">
+              <EditProduct />
+            </Route>
             <Route path="/product/new">
               <CreateProduct />
             </Route>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -70,7 +70,7 @@ function Table() {
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                       <a
-                        href="/edit"
+                        href={`product/edit/${item.productid}`}
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit
